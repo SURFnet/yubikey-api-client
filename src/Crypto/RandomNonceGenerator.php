9ab5a3fa42d2cfd370a2f72635284251e422617b
Alex Rothuis <arothuis@ibuildings.nl>
2017-02-22 13:50:06 +0100

    Fix Yubikey nonce generation
    Nonces can only be 16 to 40 character long according to the yubico specification: https://developers.yubico.com/OTP/Specifications/OTP_validation_protocol.html

--- src/Crypto/RandomNonceGenerator.php
+++ src/Crypto/RandomNonceGenerator.php
@@ -6,6 +6,6 @@ class RandomNonceGenerator implements NonceGenerator
 {
     public function generateNonce()
     {
-        return bin2hex(random_bytes(32));
+        return bin2hex(random_bytes(20));
     }
 }
