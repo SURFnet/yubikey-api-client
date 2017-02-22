962066992dc74439046a963552930e56cbc4e5c9
Alex Rothuis <arothuis@ibuildings.nl>
2017-02-22 16:17:25 +0100

    Document why nonces can only be 16 to 40 characters long
    See: https://developers.yubico.com/OTP/Specifications/OTP_validation_protocol.html

--- src/Crypto/RandomNonceGenerator.php
+++ src/Crypto/RandomNonceGenerator.php
@@ -6,6 +6,7 @@ class RandomNonceGenerator implements NonceGenerator
 {
     public function generateNonce()
     {
+        // Nonces can only be 16 to 40 character long according to the yubico specification
         return bin2hex(random_bytes(20));
     }
 }
