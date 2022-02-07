0b7ec26bf58545e6072e93a7dd6e584c9174b016
Thijs Kinkhorst <thijs@kinkhorst.com>
2022-02-07 20:43:17 +0100

    Simplify ServerPoolClient
    Yubico no longer uses an API which needs requests to be sent to
    multiple endpoints, but rather has one geolocated API endpoint.
    This removes the logic to try multiple and thereby simplifies
    the code.
    
    This is the most simplification we can do without breaking
    the library's published interface. We can do that (would probably
    mean to remove the SeverPoolClient altogether) but that breaks
    implementers for no clear gain at this point. Just simplifying
    the current code has a great benefit/cost tradeoff.

--- tests/unit/Http/ServerPoolClientTest.php
+++ tests/unit/Http/ServerPoolClientTest.php
@@ -23,48 +23,6 @@ class ServerPoolClientTest extends \PHPUnit\Framework\TestCase
         $client->get([]);
     }
 
-    public function testItTriesTwice(): void
-    {
-        $returnValues = [
-            new RequestException('Comms failure', m::mock('Psr\Http\Message\RequestInterface'), /*No response*/ null),
-            m::mock('Psr\Http\Message\ResponseInterface'),
-        ];
-
-        $client = new ServerPoolClient(
-            m::mock('GuzzleHttp\Client')
-                ->shouldReceive('get')->twice()->andReturnUsing(function () use (&$returnValues) {
-                    $r = array_shift($returnValues);
-                    if ($r instanceof RequestException) {
-                        throw $r;
-                    }
-                    return $r;
-                })
-                ->getMock()
-        );
-
-        $client->get([]);
-    }
-
-    public function testItThrowsGuzzlesExceptionAfterTryingTwice(): void
-    {
-        $this->expectException('GuzzleHttp\Exception\RequestException', 'Comms failure #2');
-
-        $exceptions = [
-            new RequestException('Comms failure #1', m::mock('Psr\Http\Message\RequestInterface'), null),
-            new RequestException('Comms failure #2', m::mock('Psr\Http\Message\RequestInterface'), null),
-        ];
-
-        $client = new ServerPoolClient(
-            m::mock('GuzzleHttp\Client')
-                ->shouldReceive('get')->twice()->andReturnUsing(function () use (&$exceptions) {
-                    throw array_shift($exceptions);
-                })
-                ->getMock()
-        );
-
-        $client->get([]);
-    }
-
     public function testItThrowsGuzzlesExceptionWhenItHasAResponse(): void
     {
         $this->expectException('GuzzleHttp\Exception\RequestException', 'Internal server error');
